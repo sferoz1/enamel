97842cc8516392b456b721491b2f7622768648c3
DoyleSean <doylesean@me.com>
2018-02-07 14:03:51 -0500

    fixed toy authoring app - added file chooser

--- Enamel/src/enamel/ToyAuthoring.java
+++ Enamel/src/enamel/ToyAuthoring.java
@@ -1,9 +1,20 @@
 package enamel;
 
+import javax.swing.JFileChooser;
+import java.io.File;
+
 public class ToyAuthoring {
 
     public static void main(String[] args) { 	  	
     	    ScenarioParser s = new ScenarioParser(true);
-    	    s.setScenarioFile("FactoryScenarios/Scenario_" + 1 + ".txt");
+    	    
+    	    JFileChooser jf = new JFileChooser();
+    	    int returnValue = jf.showOpenDialog(null);
+
+    		if (returnValue == JFileChooser.APPROVE_OPTION) {
+    			File selectedFile = jf.getSelectedFile();
+    			s.setScenarioFile(selectedFile.getAbsolutePath());
+    		}
+    	    
     }
 }
